88be05c8b6560c5ba101ab8fbd2a6d33f9f57960
ruigodoy <rui.godoy@hotmail.com>
2023-02-03 18:23:47 -0300

    Add base Config

---
+++ app/config.py
@@ -0,0 +1,14 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    jwt_secret_key: str = "erp-wallet"
+
+    class Config:
+        env_file = ".env"
+        env_file_encondig =  "utf-8"
+
+
+
+def make_settings() -> Settings:
+    return Settings()
