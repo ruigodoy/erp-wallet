da8af2cd92a1407ad2048b66f2c620c0cae720a0
ruigodoy <rui.godoy@hotmail.com>
2023-02-03 18:24:10 -0300

    Add base auth

---
+++ app/entrypoint/api/dependencies.py
@@ -0,0 +1,25 @@
+from fastapi import Depends, HTTPException, Security, status
+from fastapi.security import APIKeyHeader
+from jose import jwt, JWTError
+from app import config
+
+API_KEY_HEADER = APIKeyHeader(name="Authorization", auto_error=True)
+
+
+def auth_required(
+    settings: config.Settings = Depends(config.make_settings),
+    key: str = Security(API_KEY_HEADER)
+):
+    invalidate_auth_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate the credentials!",
+        headers={"Authenticate": "Bearer"}
+    )
+    try:
+        jwt_payload = jwt.decode(key, settings.jwt_secret_key, algorithms=["HS256"])
+        permission = jwt_payload.get("permission")
+
+        if permission is None or permission != "full-access":
+            raise invalidate_auth_exception
+    except JWTError:
+        raise invalidate_auth_exception
