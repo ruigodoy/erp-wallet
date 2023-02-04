b1a79ffcb5cff65bcf336de1998047e752d81353
ruigodoy <rui.godoy@hotmail.com>
2023-02-04 10:06:28 -0300

    Add validation of sum of products

--- app/entrypoint/api/routers/cashback.py
+++ app/entrypoint/api/routers/cashback.py
@@ -1,7 +1,5 @@
-import json
-
-from fastapi import APIRouter, Depends, Body
-from app.entrypoint.api import dependencies
+from fastapi import APIRouter, Depends, HTTPException, status
+from app.entrypoint.api import dependencies, schemas
 
 router = APIRouter(
     dependencies=[
@@ -11,5 +9,27 @@ router = APIRouter(
 
 
 @router.post("/api/cashback")
-def send_cashback(body: dict = Body(...)):
-    return body
+def cashback(cashback: schemas.CashBack) -> schemas.CashBack:
+    invalidate_sum_total = HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="The sum of the products does not equal the total!",
+        )
+    try:
+        _validate_sum_total(cashback=cashback)
+    except ValueError:
+        raise invalidate_sum_total
+
+    return cashback
+
+
+def _validate_sum_total(cashback: schemas.CashBack):
+    total = cashback.total
+    sum_total_products = 0
+    
+    for product in cashback.products:
+        sum_total_products += product.value
+    
+    if total != sum_total_products:
+        raise ValueError
+
+    return True
