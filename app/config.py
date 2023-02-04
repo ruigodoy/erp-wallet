ce184cbac1e2843eceb5c21a5f4f6c62db75001f
ruigodoy <rui.godoy@hotmail.com>
2023-02-04 14:49:44 -0300

    Add base api url

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     jwt_secret_key: str = "erp-wallet"
     url_db: str = "postgresql://erp_user:erp_pass@localhost:5432/erpwallet"
+    base_api_url: str = "https://5efb30ac80d8170016f7613d.mockapi.io"
 
     class Config:
         env_file = ".env"
