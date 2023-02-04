0043ad2b03aa427d2fa69df5a8457e56364a2d4e
ruigodoy <rui.godoy@hotmail.com>
2023-02-04 10:05:23 -0300

    Add CashBack Schema

---
+++ app/entrypoint/api/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, constr
+from datetime import datetime
+
+
+class Customer(BaseModel):
+    document: constr(regex=r"^([0-9]{3}[.]?[0-9]{3}[.]?[0-9]{3}[-]?[0-9]{2})$")
+    name: str
+
+class Product(BaseModel):
+    type: str
+    value: float
+    qty: int
+
+class CashBack(BaseModel):
+    sold_at: datetime
+    customer: Customer
+    total: float
+    products: list[Product]
